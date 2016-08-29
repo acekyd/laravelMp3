76cd5aa70a9098f893cf3bce89ecef87fdd7f364
Abati Adewale <acekyd01@gmail.com>
2016-08-29 23:08:48 +0100

    get format and restructure

--- src/LaravelMP3.php
+++ src/LaravelMP3.php
@@ -5,31 +5,34 @@ namespace Acekyd\LaravelMP3;
 class LaravelMP3
 {
 
-	public function load()
+	public function load($path = null)
 	{
 		include('getid3/getid3.php');
 		$getID3 = new \getID3;
-		return $getID3;
+		$info = $getID3->analyze( $path );
+		if(!isset($info['audio']))
+	    {
+	        return $info;
+	    }
+		return $info['audio'];
 	}
 
     public function hello()
     {
     	$lib = $this->load();
     	return $lib;
-        return 'Hello Ace!';
     }
 
     public function getBitrate($path)
     {
-    	$lib = $this->load();
-    	$info = $lib->analyze( $path );
+    	$lib = $this->load($path);
+	    return $lib['bitrate'];
+    }
 
-	    if(!isset($info['audio']))
-	    {
-	        return $info;
-	    }
-
-	    return $info['audio']['bitrate'];
+    public function getFormat($path)
+    {
+    	$lib = $this->load($path);
+	    return $lib['dataformat'];
     }
 }
 
