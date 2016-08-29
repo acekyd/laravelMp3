38ff1acb096267a29f1eaeaf259a44154164247e
Abati Adewale <acekyd01@gmail.com>
2016-08-29 22:43:55 +0100

    getID3 library

--- src/LaravelMP3.php
+++ src/LaravelMP3.php
@@ -4,8 +4,12 @@ namespace Acekyd\LaravelMP3;
 
 class LaravelMP3
 {
+	
     public function hello()
     {
+    	include('getid3/getid3.php');
+    	$getID3 = new \getID3;
+    	return $getID3;
         return 'Hello Ace!';
     }
 }
